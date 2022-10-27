77e526a7e0f7355ba9848b4889aa34a5a1743922
RahafTwq <raaldosary22@twq.idserve.net>
2022-10-27 09:54:53 +0300

    Initial Commit

---
+++ labPresentme/labPresentmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  labPresentmeApp.swift
+//  labPresentme
+//
+//  Created by Rahaf Aldosary on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct labPresentmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
