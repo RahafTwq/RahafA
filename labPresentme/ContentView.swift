22918ac81dcfc67878bfc24d91471c5a42c60b11
RahafTwq <raaldosary22@twq.idserve.net>
2022-10-27 10:36:52 +0300

    lab

--- labPresentme/ContentView.swift
+++ labPresentme/ContentView.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            
+            VStack {
+                ZStack{
+                    Image("me")
+                        .frame(width: 300,height: 290)
+                        .clipShape(Circle())
+                    
+                    Circle()
+                        .strokeBorder(.blue, lineWidth: 7)
+                        .frame(width: 300,height: 290)
+                }
+                
+                Text("Rahaf Aldosary")
+                    .foregroundColor(.blue)
+                    .font(.largeTitle)
+                Text("I love video games and i'm really excited to improve my programming skills and meet new friends 👓")
+                    .multilineTextAlignment(.center)
+                    .font(.subheadline)
+                
+                
+            }
+            .padding()
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
